a229dcb79abd80a92dd712f6d4d1290f82ea46dc
Micro9261 <140260920+Micro9261@users.noreply.github.com>
2025-12-24 13:26:05 +0100

    Class Game creation

---
+++ include/Game.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+class Game final 
+{
+public:
+  Game();
+  void Start();
+  ~Game();
+private:
+
+};
---
+++ src/Game.cpp
@@ -0,0 +1,16 @@
+#include "Game.hpp"
+
+Game::Game()
+{
+  std::cout << "Game() cot\n";
+}
+
+void Game::Start()
+{
+  std::cout << "Game::Start()\n";
+}
+
+Game::~Game()
+{
+  std::cout << "Game::~Game()\n";
+}
